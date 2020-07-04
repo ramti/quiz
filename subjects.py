b06b606d5b1b96b660ffadafb509cbfe405e4511
Itamar Shiryan <itamar.shiryan@gmail.com>
2020-07-04 18:48:02 +0300

    Added bio2 questions

--- subjects.py
+++ subjects.py
@@ -11,7 +11,7 @@ class Subject:
 
 SUBJECTS = [
     Subject(name="bio1", display_name="מבוא לביולוגיה א'", question_db="data/questions-bio1.csv", cards_db="data/cards-bio1.csv"),
-    Subject(name="bio2", display_name="מבוא לביולוגיה ב'", question_db="", cards_db="data/cards-bio2.csv"),
+    Subject(name="bio2", display_name="מבוא לביולוגיה ב'", question_db="data/questions-bio2.csv", cards_db="data/cards-bio2.csv"),
     Subject(name="bio3", display_name="מבוא לביולוגיה ג'", question_db="", cards_db="data/cards-bio3.csv")
 ]
 
